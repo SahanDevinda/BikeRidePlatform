82af67e3e606b3f7e740bb65250d5dfafa766b95
Aathif1118 <aathifahamed66@gmail.com>
2026-05-21 22:45:10 +0530

    initial commit

---
+++ src/main/java/com/bikeride/Bike_Ride_Platform/BikeRidePlatformApplication.java
@@ -0,0 +1,13 @@
+package com.bikeride.Bike_Ride_Platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikeRidePlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BikeRidePlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bikeride/Bike_Ride_Platform/BikeRidePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bikeride.Bike_Ride_Platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BikeRidePlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
